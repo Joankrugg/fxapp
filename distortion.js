6ae87a9a22ec4c46167784a26e55bd885668d0e3
Joankrugg <joan_krugg@live.fr>
2025-06-24 14:00:37 +0200

    danelectro

--- distortion.js
+++ distortion.js
@@ -38,12 +38,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Fonction de distorsion
     function makeDistortionCurve(amount) {
+      const k = typeof amount === 'number' ? amount : 50;
       const n_samples = 44100;
       const curve = new Float32Array(n_samples);
       const deg = Math.PI / 180;
+      let x;
       for (let i = 0; i < n_samples; ++i) {
-        const x = (i * 2) / n_samples - 1;
-        curve[i] = (amount * x) / (1 + Math.abs(x)); // Courbe de distorsion
+        x = (i * 2) / n_samples - 1;
+        curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
       }
       return curve;
     }
